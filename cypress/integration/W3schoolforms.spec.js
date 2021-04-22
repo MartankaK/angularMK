7a82acb90b1f61ed088000b6fabbc755c98c9baf
MartankaK <mata.kohutova.23@gmail.com>
2021-04-22 11:34:40 +0200

    make e2e tests for W3schoolforms

---
+++ cypress/integration/W3schoolforms.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+const { Input } = require("@angular/core")
+
+context('w3schools', () => {
+  beforeEach(() => {
+    cy.visit('https://www.w3schools.com/html/html_forms.asp')
+    cy.contains('Accept all').click()
+  })
+
+  describe('Implicit Assertions', () => {
+    it('.should() - make an assertion about the current subject', () => {
+      cy.get('.w3-white')
+        .find('form label:first')
+        .should('contain', 'First name')
+      cy.get('form')
+        .get('input[name="fname"]')
+        .first()
+        .clear()
+        .type('Martina')
+      cy.get('form')
+        .get('input[name="lname"]')
+        .first()
+        .clear()
+        .type('Kohutova')
+      cy.get('form')
+        .get('input[value="Submit"]')
+        .first()
+        .click()
+    })
+  })
+
+})
