a6aaa29b23af9f356fe7734b2e7edbc920c91aea
MartankaK <mata.kohutova.23@gmail.com>
2021-04-26 23:49:08 +0200

    make e2e test - find the user profile on github

---
+++ cypress/integration/github.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+const { Input } = require("@angular/core")
+
+context('github', () => {
+  beforeEach(() => {
+    cy.visit('https://github.com/')
+  })
+
+  describe('as an offline user', () => {
+    it('should find the user profile', () => {
+      cy.get('.js-details-target')
+        .first()
+        .click()
+      cy.get('form')
+        .get('input[name="q"]')
+        .type('MartankaK{enter}')
+      cy.get('.menu-item')
+        .contains('Users')
+        .click()
+      cy.wait(500)
+      cy.get('.codesearch-results')
+        .contains('MartankaK')
+        .click()
+      cy.url().should('eq', 'https://github.com/MartankaK')
+    })
+  })
+})
